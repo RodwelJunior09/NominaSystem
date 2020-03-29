db729e480e786737252ce7852f7c30e6695ec614
RodwelJunior09 <me@Mes-MacBook-Pro.local>
2020-03-28 23:08:53 -0400

    Delete Project Task

--- routes.py
+++ routes.py
@@ -1,19 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import data.services.data_services as srv
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
-@app.route('/')
-def hello_world():
-    return render_template("homePage.html")
+@app.route('/', methods=["GET"])
+def homePage():
+    collection_projects = srv.find_all_projects()
+    return render_template("homePage.html", title="HomePage", projects=collection_projects)
+
+@app.route('/delete/<object_id>', methods=["GET", "POST"])
+def deleteProject(object_id):
+    if request.method == "POST":
+        srv.delete_project(object_id)
+        print("Project Deleted")
+        return redirect('/')
+    else:
+        return render_template("DeleteProject.html")
 
 @app.route('/project_registration', methods=["GET", "POST"]) # Timeout because of a ssl error - maybe a mac issue.
-def project_name():
+def projectRegistration():
     if request.method == "POST":
         srv.create_project(request.form.get('project-title'), request.form.get('owner-name'))
         print("Project Saved")
-    return render_template("project_name.html")
+        return redirect("/")
+    return render_template("ProjectRegistration.html")
 
 @app.route('/employee_registration', methods=["GET", "POST"])
 def employeeRegistration():
-    return render_template("employee_register.html")
+    return render_template("EmployeeRegistration.html")
